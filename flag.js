0683c046d897510445617e50061a7f0d16a4ee4a
DELL <shivanikumari28@gmail.com>
2023-07-31 00:39:27 +0530

    initial commit

---
+++ flag.js
@@ -0,0 +1,46 @@
+function brazilFunc() {
+  var c = "This is the flag of Brazil";
+  document.getElementById("brazil").innerHTML = c;
+}
+
+function outBrazil() {
+  var c = "";
+  document.getElementById("brazil").innerHTML = c;
+}
+
+function indiaFunc() {
+  var c = "This is the flag of India";
+  document.getElementById("india").innerHTML = c;
+}
+
+function outIndia() {
+  var c = "";
+  document.getElementById("india").innerHTML = c;
+}
+
+function argFunc() {
+  var c = "This is the flag of Argentina";
+  document.getElementById("arg").innerHTML = c;
+}
+function outArg() {
+  var c = "";
+  document.getElementById("arg").innerHTML = c;
+}
+
+function skoreaFunc() {
+  var c = "This is the flag of South Korea";
+  document.getElementById("skorea").innerHTML = c;
+}
+function outSkorea() {
+  var c = "";
+  document.getElementById("skorea").innerHTML = c;
+}
+
+function nepalFunc() {
+  var c = "This is the flag of Nepal";
+  document.getElementById("nepal").innerHTML = c;
+}
+function outNepal() {
+  var c = "";
+  document.getElementById("nepal").innerHTML = c;
+}
